d9881d33fa791a772c92c666148e23adfe264c8c
Giancarlo Anemone <ganemone@uber.com>
2019-03-26 11:32:07 -0400

    Base Case

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider, connect} from 'react-redux';
+import {BrowserRouter, Route, Switch, Link, withRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+
+const reducer = (state, action) => {
+  return state || {
+    test: 'data',
+    hello: 'world',
+  }
+}
+
+const store = createStore(reducer);
+
+function Test() {
+  return <div>Test</div>
+}
+
+function Hello() {
+  return <div>Hello</div>
+}
 
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          <li><Link to="/test">Test</Link></li>
+          <li><Link to="/hello">Hello</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/test" component={Test}/>
+          <Route path="/hello" component={Hello}/>
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const AppContainer = connect(state => state)(App);
+// Wrapping in withRouter will ensure the component is updated
+// const AppContainer = withRouter(connect(state => state)(App));
+
+export default function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppContainer />
+      </BrowserRouter>
+    </Provider>
+  );
+}
